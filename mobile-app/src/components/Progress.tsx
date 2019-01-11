4967c76c976cae1cb841fc9f85c361d0283a85fb
csteinberg <csteinberg@geekhive.com>
2019-01-11 16:56:21 -0500

    Working on progress bars

--- mobile-app/src/components/Progress.tsx
+++ mobile-app/src/components/Progress.tsx
@@ -1,17 +1,36 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../appearance/styles';
 
 export interface ProgressProps {
   count: number;
 }
 
-export const Progress: React.SFC<ProgressProps> = (props) => (
-  <View>
-    <Text>Progress</Text>
-  </View>
+export class Progress extends React.Component<ProgressProps> {
+  public render() {
+    const { count } = this.props;
+    const bars: JSX.Element[] = [];
+    for (let i = 0; i < count; i++) {
+      bars.push(
+        <View style={styles.bar} key={i} />
+      );
+    }
+
+    return (
+      <View>
+        {bars}
+      </View>
+    );
+  }
+}
+
+export const Bar: React.SFC = () => (
+  <View style={styles.bar} />
 );
 
 const styles = StyleSheet.create({
-
+  bar: {
+    borderBottomColor: colors.neonPink,
+    borderBottomWidth: 1,
+  }
 });
-
